48b99ef89ba8d4957603e13cef6aae4cfb83a540
David Mtz <35812511+DavidMtz1@users.noreply.github.com>
2020-03-11 16:20:47 -0700

    Update Practice5.R

--- Practices/Practice5.R
+++ Practices/Practice5.R
@@ -10,3 +10,18 @@ stats[stats$Income.Group == "middle income","Country.Name"]
 
 #Filter countries by Low income
 stats[stats$Income.Group == "Low income","Country.Name"]
+
+#Filter countries by Upper middle income
+stats[stats$Income.Group == "Upper middle income","Country.Name"]
+
+#Filter by countrie Malta
+stats[stats$Country.Name == "Malta",]
+
+#Filter by countrie Qatar
+stats[stats$Country.Name == "Qatar",]
+
+#Filter by countrie Netherlands
+stats[stats$Country.Name == "Netherlands",]
+
+#Filter by countrie Norway
+stats[stats$Country.Name == "Norway",]
