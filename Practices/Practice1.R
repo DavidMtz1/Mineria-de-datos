2084e1223290a613faf46bb79243d71ed3fc0164
KarenMorales16 <47936628+KarenMorales16@users.noreply.github.com>
2020-03-11 16:06:18 -0700

    Practice1

--- Practices/Practice1.R
+++ Practices/Practice1.R
@@ -12,3 +12,21 @@ n<-10000
 #2. Initialize counter
 contador <-0
 resultado <-0
+  
+#3. loop for(i in rnorm(size))
+for(i in 1:n)
+{
+  Norm <- rnorm(i, mean = 0, sd = 1)
+  
+#4. Compruebe si la variable iterada cae
+  if(Norm >=-1 && Norm  <=1){
+    #5. Aumente el contador si la condición es verdadera
+    contador<- contador+1
+ 
+  }
+}
+
+#6. devolver un resultado <- contador / N
+resultado <-contador/n
+print(contador)
+print(resultado) 
